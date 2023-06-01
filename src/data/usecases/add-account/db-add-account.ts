c91c9c06ee96206ec41806dc6f9b24973fb769b6
Luiz Henrique <luizhjramos@outlook.com>
2023-05-31 22:17:28 -0300

    refactor: group all DbAddAccount usecase protocols in a single file

--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -1,9 +1,9 @@
-import { type Encrypter } from '../../protocols/encrypter'
-import { type AccountModel } from '../../../domain/models/account'
 import {
   type AddAccountModel,
-  type AddAccount
-} from '../../../domain/usecases/add-account'
+  type AddAccount,
+  type Encrypter,
+  type AccountModel
+} from './db-add-account-protocols'
 
 export class DbAddAccount implements AddAccount {
   constructor (private readonly encrypter: Encrypter) {}
